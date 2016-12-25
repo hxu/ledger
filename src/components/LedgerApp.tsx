d87b422c3ba3e97f0558c7eccc5e8d701cf274ad
Han Xu <hgcrpd@gmail.com>
2016-12-25 22:04:22 +0800

    unnecessary props

--- src/components/LedgerApp.tsx
+++ src/components/LedgerApp.tsx
@@ -1,12 +1,6 @@
 import * as React from "react";
 
-export interface LedgerProps {foo: string}
-
-export class LedgerApp extends React.Component<LedgerProps, undefined> {
-    constructor(props: LedgerProps) {
-        super(props);
-    }
-
+export class LedgerApp extends React.Component<undefined, undefined> {
     render() {
         return (
             <div>
--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,6 @@ import * as ReactDOM from "react-dom";
 import {LedgerApp} from "./components/LedgerApp";
 
 ReactDOM.render(
-    <LedgerApp foo="bar" />,
+    <LedgerApp />,
     document.getElementById("app")
 );
